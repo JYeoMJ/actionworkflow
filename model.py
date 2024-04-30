37aafd7564e5cb6f68f08f2a14193e34e7f808dc
Yeo Ming Jie Jonathan <106881433+JYeoMJ@users.noreply.github.com>
2024-04-30 11:57:31 +0800

    Update model.py

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ def main():
 	y = data.target
 
 	# Train-test split of data
-	X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state+42)
+	X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
 	
 	# Create and train the model
